664a35dee9d56dd90c7838f3765bde8daef61868
Motorolpe <mauriciochulau@gmail.com>
2026-02-25 09:04:00 -0500

    Preparando para subir a render

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-CADENA_CONEXION = "postgresql://gastos:gastos@localhost:5432/bd_gastos"
+#CADENA_CONEXION = "postgresql://gastos:gastos@localhost:5432/bd_gastos"
+CADENA_CONEXION = os.getenv("DATABASE_URL")
 
 engine = create_engine(CADENA_CONEXION)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
